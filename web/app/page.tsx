baeb16e83c2138a95398368f286e0c96bb8b9908
abalinda <balinda.centar@gmail.com>
2025-12-03 15:58:20 +0100

    added Posthog analytics

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,6 +2,14 @@ import { supabase } from "@/lib/supabase";
 import Link from "next/link";
 import { CategoryNav, NavBar } from "./_components/navigation";
 
+//-- POSTHOG ANALYTICS SETUP --
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+  api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+  defaults: '2025-11-30'
+});
+
 // Revalidate every 60 seconds
 export const revalidate = 60;
 
