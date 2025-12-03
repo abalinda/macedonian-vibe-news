802c65b946c91d67d2a279e8c70411e1c438871f
abalinda <balinda.centar@gmail.com>
2025-12-03 16:13:09 +0100

    configuring posthog correctly. Added instrumentation-client.ts file in /web

---
+++ web/instrumentation-client.ts
@@ -0,0 +1,9 @@
+//-- POSTHOG ANALYTICS SETUP --
+
+import posthog from 'posthog-js'
+
+posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    defaults: '2025-11-30'
+});
+            
