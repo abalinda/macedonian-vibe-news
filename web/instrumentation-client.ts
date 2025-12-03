3d12d12b58eb24b969d9aa396d9f3a1b7a807309
abalinda <balinda.centar@gmail.com>
2025-12-03 21:57:49 +0100

    Changed api_host at instrumentation-client.ts back to correct api host. PostHog works

--- web/instrumentation-client.ts
+++ web/instrumentation-client.ts
@@ -3,8 +3,8 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    // api_host: '/relay-Z6aO/',
-    api_host: 'https://webhook.site/f63a1af5-2df8-4d86-aac1-36bb3b2b244f',
+    api_host: '/relay-Z6aO/',
+    // api_host: 'https://webhook.site/f63a1af5-2df8-4d86-aac1-36bb3b2b244f',
     ui_host: 'https://eu.posthog.com',
     defaults: '2025-11-30',
 });
