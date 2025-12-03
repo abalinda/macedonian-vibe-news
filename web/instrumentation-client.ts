2fdbb98ebe754e7aeeb9a860a38c06755d615dcf
abalinda <balinda.centar@gmail.com>
2025-12-03 21:42:25 +0100

    Changed api_host at instrumentation-client.ts just to test something out. PostHog will break

--- web/instrumentation-client.ts
+++ web/instrumentation-client.ts
@@ -3,7 +3,8 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: '/relay-Z6aO/',
+    // api_host: '/relay-Z6aO/',
+    api_host: 'https://thainamansurpsi.com',
     ui_host: 'https://eu.posthog.com',
     defaults: '2025-11-30',
 });
