2e1eb9397a89f210beb9815e1a98d539649ed1a1
abalinda <balinda.centar@gmail.com>
2025-12-03 16:27:57 +0100

    editing inst-client.ts

--- web/instrumentation-client.ts
+++ web/instrumentation-client.ts
@@ -3,7 +3,8 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
-    defaults: '2025-11-30'
+    api_host: '/relay-Z6aO/',
+    ui_host: 'https://eu.posthog.com',
+    defaults: '2025-11-30',
 });
             
