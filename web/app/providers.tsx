99585e2987c885c2979f10ead5c50e3d0842b317
abalinda <balinda.centar@gmail.com>
2025-12-06 22:40:35 +0100

    forgot to save the file...

--- web/app/providers.tsx
+++ web/app/providers.tsx
@@ -1,9 +1,31 @@
+// 'use client'
+// import posthog from 'posthog-js'
+// import { PostHogProvider} from 'posthog-js/react'
+// import { useEffect } from 'react'
+
+// export function PostHogProvider({ children }: { children: React.ReactNode }) {
+//   useEffect(() => {
+//     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+//       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+//       defaults: '2025-05-24',
+//       opt_in_site_apps: true,
+//       person_profiles: 'identified_only',
+//     })
+//   }, []);
+
+//   return <PostHogProvider client={posthog}>
+//     {children}
+//     </PostHogProvider>
+// }
+
+
+// app/providers.tsx
 'use client'
 import posthog from 'posthog-js'
-import { PostHogProvider} from 'posthog-js/react'
+import { PostHogProvider as PHProvider } from 'posthog-js/react'
 import { useEffect } from 'react'
 
-export function PHProvider({ children }: { children: React.ReactNode }) {
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
   useEffect(() => {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
       api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
@@ -13,7 +35,7 @@ export function PHProvider({ children }: { children: React.ReactNode }) {
     })
   }, []);
 
-  return <PostHogProvider client={posthog}>
+  return <PHProvider client={posthog}>
     {children}
-    </PostHogProvider>
+    </PHProvider>
 }
