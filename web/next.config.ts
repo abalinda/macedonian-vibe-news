899046d10c7aa892c97c3256690a5650795496c0
abalinda <balinda.centar@gmail.com>
2026-01-21 22:07:40 +0100

    chore: migrate from vercel to cloudflare opennext

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
+
+
+initOpenNextCloudflareForDev();
 
 const nextConfig: NextConfig = {
   /* config options here */
-  skipTrailingSlashRedirect: true, 
+  images: {
+    unoptimized: true, // Keep this until you set up Cloudflare Images binding
+  },
+  transpilePackages: ["@libsql/client"],
 };
 
 export default nextConfig;
