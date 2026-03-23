d7182f7f398cd90366173da45af502f7e0da87cc
abalinda <balinda.centar@gmail.com>
2026-03-23 14:44:54 +0100

    push to deploy

--- web/instrumentation-client.ts
+++ web/instrumentation-client.ts
@@ -3,8 +3,6 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-    // api_host: 'https://eu.i.posthog.com',
     api_host: 'https://t.vibes.mk',
-    // api_host: 'https://webhook.site/f63a1af5-2df8-4d86-aac1-36bb3b2b244f',
     defaults: '2026-01-30'
 });
