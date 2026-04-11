33c888973b08c30351657a0fe9d0ccee3a4b482a
abalinda <balinda.centar@gmail.com>
2026-04-11 16:34:21 +0200

    changing a small thing to see if the .dev preview actually works

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -38,7 +38,7 @@ const CATEGORY_LABELS = {
   Lifestyle: "Животен стил",
   Business: "Бизнис",
   Sports: "Спорт",
-  Blog: "Блог",
+  Blog: "Тест",
 } as const;
 
 const toPlain = (value: any) => JSON.parse(JSON.stringify(value));
