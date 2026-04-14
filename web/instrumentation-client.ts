bf42a115b4e9d9385381ac39b1b737d5f00a9e32
abalinda <balinda.centar@gmail.com>
2026-04-14 12:36:47 +0200

    disabling reverse proxy temporarily

--- web/instrumentation-client.ts
+++ web/instrumentation-client.ts
@@ -2,7 +2,8 @@
 import posthog from 'posthog-js'
 
 posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {  
-  api_host: 'https://t.vibes.mk',  
+  // api_host: 'https://t.vibes.mk',  
+  api_host: 'https://eu.posthog.com',  
   ui_host: 'https://eu.posthog.com',  
   defaults: '2026-01-30',  
   opt_in_site_apps: true,  
